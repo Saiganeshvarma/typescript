4705334eb0b348e42c7c6fa1bc39dfda2500b53c
saiganeshpalla <ganesh@stockholmitacademy.org>
2025-03-19 17:08:09 +0530

    added the code

---
+++ varibles.js
@@ -0,0 +1,33 @@
+// number
+var a = 10;
+console.log(a);
+// string
+var b = "sai ganesh varma";
+console.log(b);
+// undefined
+var c;
+console.log(c);
+// null
+var d = null;
+console.log(d);
+// object
+var obj = {
+    name: 'sai',
+};
+// array with numbers
+var arr = [10, 20, 30, 40];
+console.log(arr);
+// array with strings
+var str = ["a", "b", "c", "d"];
+console.log(str);
+var newStr = ["a", "b", "c", "d"];
+console.log(newStr);
+// any 
+var anyStr = "sai";
+var anyNum = 10;
+var anyObj = {
+    name: "sai"
+}
+
+
+
---
+++ varibles.ts
@@ -0,0 +1,50 @@
+// number
+var a : number = 10
+console.log(a);
+// string
+
+var b : string = "sai ganesh varma"
+console.log(b);
+
+// undefined
+
+var c : undefined
+console.log(c);
+
+
+// null
+
+var d : null = null
+console.log(d);
+// object
+
+var obj : object = {
+    name : 'sai',
+
+}
+// array with numbers
+
+var arr : number[] = [10,20,30,40]
+console.log(arr);
+
+// array with strings
+
+var str : string[] = ["a","b","c","d"]
+console.log(str);
+
+var newStr : Array <string> = ["a","b","c","d"]
+console.log(newStr);
+
+// any 
+var anyStr : any = "sai"
+
+var anyNum : any = 10
+
+
+var anyObj : any = {
+    name : "sai"
+}
+
+export{}
+
+
