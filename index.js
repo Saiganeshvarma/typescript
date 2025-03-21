13b6fd16c380fd2f729405923b167f4a6693d724
saiganeshpalla <ganesh@stockholmitacademy.org>
2025-03-21 12:09:55 +0530

    added thr code

--- index.js
+++ index.js
@@ -1,39 +1,78 @@
 "use strict";
+// import { c } from "vite/dist/node/moduleRunnerTransport.d-CXw_Ws6P"
 Object.defineProperty(exports, "__esModule", { value: true });
-// varibles
+// // varibles
+// var a : number|string|boolean|number[]
+// a = 10
+// a = "hello"
+// a = true
+// a = [1,2,3,4,4]
+// // functions
+// function check(a: number , b : number): boolean | string{
+//     if(a>b){
+//         return true
+//     }else{
+//         return "no"
+//     }
+// }
+// // void
+// function dontReturn(str : string) : void{
+//     console.log("donot return anything");
+// }
+// dontReturn("")
+// // never
+// function neverReturn(error : string) : never{
+//     throw new Error(error)
+// }
+// // type aliases
+// type User = {
+//     name : string | number | boolean,
+//     email : string,
+//     phoen : number,
+//     city : string
+// }
+// function createUser(user : User){
+// }
+// createUser({name : 465765,email : "",phoen : 475645, city:""})
+// var arr : number[] = [1,2,3,4,5]
+// var strArr : string[] = ["sai","ganesh","varma"]
+// var arrayWithNumStr : (number|string)[] = ["sai","ganesh",1,2,3,4]
+// arrayWithNumStr.map((item)=>{
+//     if(typeof(item) == "string"){
+//         console.log(`${item} is the string`)
+//     }else if(typeof(item) == "number"){
+//         console.log(`${item} is the number`);
+//     }
+// })
+// // twodimensioanl array
+// var twodArray : (number|string)[][] = [
+//     [1,2,3,4,5],
+//     [1,2,3,4],
+//     ["sai"]
+// ]
 var a;
 a = 10;
-a = "hello";
-a = true;
-a = [1, 2, 3, 4, 4];
-// functions
-function check(a, b) {
-    if (a > b) {
+a = "sai ganesh";
+var arr = [1, 2, 3, 4, 5];
+var arrStr = ["sai", "ganesh", "varma"];
+var arrWithNumStr = [1, 2, 3, 4, "sai", "ganesh", "varma"];
+arrWithNumStr.map(function (item) {
+    if (typeof (item) == "string") {
         return true;
     }
-    else {
-        return "no";
-    }
-}
-// void
-function dontReturn(str) {
-    console.log("donot return anything");
-}
-// never
-function neverReturn(error) {
-    throw new Error(error);
-}
-function createUser(user) {
-}
-createUser({ name: 465765, email: "", phoen: 475645, city: "" });
-var arr = [1, 2, 3, 4, 5];
-var strArr = ["sai", "ganesh", "varma"];
-var arrayWithNumStr = ["sai", "ganesh", 1, 2, 3, 4];
-arrayWithNumStr.map(function (item) {
-    if (typeof (item) == "string") {
-        console.log("".concat(item, " is the string"));
-    }
     else if (typeof (item) == "number") {
-        console.log("".concat(item, " is the number"));
+        return false;
     }
 });
+function check(a, b) {
+    if (a > b) {
+        return "yes";
+    }
+    else {
+        return false;
+    }
+}
+console.log(check(10, 20));
+function createUser(User) {
+}
+console.log(createUser({ myname: "", city: "", age: 18 }));
--- index.ts
+++ index.ts
@@ -1,66 +1,110 @@
-import { c } from "vite/dist/node/moduleRunnerTransport.d-CXw_Ws6P"
+// import { c } from "vite/dist/node/moduleRunnerTransport.d-CXw_Ws6P"
 
-// varibles
-var a : number|string|boolean|number[]
+// // varibles
+// var a : number|string|boolean|number[]
+// a = 10
+// a = "hello"
+// a = true
+// a = [1,2,3,4,4]
+
+// // functions
+// function check(a: number , b : number): boolean | string{
+//     if(a>b){
+//         return true
+//     }else{
+//         return "no"
+//     }
+
+// }
+// // void
+// function dontReturn(str : string) : void{
+//     console.log("donot return anything");
+
+// }
+// dontReturn("")
+
+// // never
+
+// function neverReturn(error : string) : never{
+//     throw new Error(error)
+
+
+
+// }
+
+// // type aliases
+
+// type User = {
+//     name : string | number | boolean,
+//     email : string,
+//     phoen : number,
+//     city : string
+// }
+
+// function createUser(user : User){
+
+// }
+// createUser({name : 465765,email : "",phoen : 475645, city:""})
+
+// var arr : number[] = [1,2,3,4,5]
+// var strArr : string[] = ["sai","ganesh","varma"]
+// var arrayWithNumStr : (number|string)[] = ["sai","ganesh",1,2,3,4]
+// arrayWithNumStr.map((item)=>{
+//     if(typeof(item) == "string"){
+//         console.log(`${item} is the string`)
+//     }else if(typeof(item) == "number"){
+//         console.log(`${item} is the number`);
+//     }
+// })
+
+// // twodimensioanl array
+
+// var twodArray : (number|string)[][] = [
+//     [1,2,3,4,5],
+//     [1,2,3,4],
+//     ["sai"]
+// ]
+
+
+var a : number | string
 a = 10
-a = "hello"
-a = true
-a = [1,2,3,4,4]
-
-// functions
-function check(a: number , b : number): boolean | string{
-    if(a>b){
-        return true
-    }else{
-        return "no"
-    }
-
-}
-// void
-function dontReturn(str : string) : void{
-    console.log("donot return anything");
-
-}
-dontReturn("")
-
-// never
-
-function neverReturn(error : string) : never{
-    throw new Error(error)
-
-
-
-}
-
-// type aliases
-
-type User = {
-    name : string | number | boolean,
-    email : string,
-    phoen : number,
-    city : string
-}
-
-function createUser(user : User){
-
-}
-createUser({name : 465765,email : "",phoen : 475645, city:""})
+a = "sai ganesh"
 
 var arr : number[] = [1,2,3,4,5]
-var strArr : string[] = ["sai","ganesh","varma"]
-var arrayWithNumStr : (number|string)[] = ["sai","ganesh",1,2,3,4]
-arrayWithNumStr.map((item)=>{
+var arrStr : string[] = ["sai","ganesh","varma"]
+var arrWithNumStr : (number|string)[] = [1,2,3,4,"sai","ganesh","varma"]
+arrWithNumStr.map((item)=>{
     if(typeof(item) == "string"){
-        console.log(`${item} is the string`)
+        return true
     }else if(typeof(item) == "number"){
-        console.log(`${item} is the number`);
+        return false
+
     }
 })
 
-// twodimensioanl array
 
-var twodArray : (number|string)[][] = [
-    [1,2,3,4,5],
-    [1,2,3,4],
-    ["sai"]
-]
+function check(a : number , b : number) : boolean| string{
+    if(a>b){
+        return "yes"
+    }else{
+        return false
+    }
+}
+
+console.log(check(10,20));
+
+export{}
+
+
+type user = {
+    myname : string,
+    city : string,
+    age : number
+
+}
+
+function createUser(User : user){
+
+}
+
+console.log(createUser({myname : "",city : "",age : 18}));
