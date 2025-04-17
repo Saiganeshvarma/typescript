324c5e1053dcc700053f753b652e55455ca8a46f
saiganeshpalla <ganesh@stockholmitacademy.org>
2025-04-17 18:35:11 +0530

    added the code

--- class.js
+++ class.js
@@ -1,52 +1,10 @@
-// // // class Person{
-// // //     name : string|number
-// // //     age : number|"yes"
-// // //     constructor(name,age){
-// // //         this.name = name,
-// // //         this.age = age
-// // //     }
-// // // }
-// // // class student extends Person{
-// // //     grade : number | string
-// // //     constructor(name : string, age : number,grade : number|string){
-// // //         super(name,age)
-// // //         this.grade = grade
-// // //     }
-// // // }
-// // // var details = new student("js",10,10)
-// // // details.grade = "yes"
-// // // export{}
-// // class details{
-// //     name : string
-// //     city : string
-// //     number : number
-// //     constructor(name,city,number){
-// //         this.name = name
-// //         this.city =city
-// //         this.number = number
-// //     }
-// // }
-// // var data = new details("","",9)
-// // console.log(data);
-// class details{
-//     name : string
-//     age : number
-//   readonly city : string
-//     constructor(name,age,city){
-//         this.name = name
-//         this.age = age
-//         this.city = city
-//     }
-// }
-// var data = new details("","",34)
-var details = /** @class */ (function () {
-    function details(name, age) {
+var students = /** @class */ (function () {
+    function students(name, age, city, isActive) {
         this.name = name;
         this.age = age;
-        this.name = name;
-        this.age = age;
+        this.city = city;
     }
-    return details;
+    return students;
 }());
-var data = new details("", 6446);
+var data = new students("", 10, "", true);
 console.log(data);
--- class.ts
+++ class.ts
@@ -1,74 +1,25 @@
-
-
-// // // class Person{
-// // //     name : string|number
-// // //     age : number|"yes"
-
-// // //     constructor(name,age){
-// // //         this.name = name,
-// // //         this.age = age
-
-// // //     }
-// // // }
-
-// // // class student extends Person{
-// // //     grade : number | string
-// // //     constructor(name : string, age : number,grade : number|string){
-// // //         super(name,age)
-// // //         this.grade = grade
-
-// // //     }
-// // // }
-
-// // // var details = new student("js",10,10)
-// // // details.grade = "yes"
-// // // export{}
-
-// // class details{
-// //     name : string
-// //     city : string
-// //     number : number
-// //     constructor(name,city,number){
-// //         this.name = name
-// //         this.city =city
-// //         this.number = number
-
-// //     }
-// // }
-
-// // var data = new details("","",9)
-// // console.log(data);
-
-
-// class details{
-//     name : string
-//     age : number
-//   readonly city : string
-//     constructor(name,age,city){
-//         this.name = name
-//         this.age = age
-//         this.city = city
-//     }
-// }
-
-// var data = new details("","",34)
-
-class details{
-    private course_count : number
-    constructor(public name : string,public age :number){
+class students{
+    name : string
+    age : number
+    city : string
+    isActive : boolean
+    constructor(name,age,city,isActive){
         this.name = name
         this.age = age
-
-    }
-    get getAppleEmail() : string{
-        return `apple${this.name}`
-
-    }
-    set  courseCount(courseNum : number){
-        this.course_count= courseNum
-
+        this.city = city
     }
 }
 
-var data = new details("",6446)
+class newData extends students{
+    grade : string
+    constructor(name,age,city,isActive){
+        super(name,age,city,isActive)
+        this.grade = this.grade
+    }
+}
+
+var data = new newData("",10,"",true,)
+
 console.log(data);
+
+export{}
